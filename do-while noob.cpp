61836f81ccbb86ea64dea052a8391bb38df47b5e
TayyabFury <the.real.tayyab.saeed@gmail.com>
2026-09-20 01:58:55 +0500

    Add files via upload

---
+++ do-while noob.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 5;
+
+    do {
+        cout << "Count is: " << count << endl;
+        count++;
+    } while (count <= 3);
+
+    return 0;
+}
